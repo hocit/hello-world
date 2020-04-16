d2705a474a17045b9db16972d61329df65e92537
Hưng <nguyentrunghung.19091989@gmail.com>
2020-04-16 17:27:39 +0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,292 @@
 import React from "react"
+import "./styles.scss"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <>
+    <header className="c-header fixed-top">
+      <div className="c-header-body">
+        <div className="c-header-logo">HOCIT.VN</div>
+        <ul className="c-nav">
+          <li className="c-nav-item">
+            <a href="" className="c-nav-link">
+              <i className="c-icon c-icon-new mr-2"></i>
+              Bài viết
+            </a>
+          </li>
+          <li className="c-nav-item">
+            <a href="" className="c-nav-link">
+              <i className="c-icon c-icon-question mr-2"></i>
+              Câu hỏi
+            </a>
+          </li>
+          <li className="c-nav-item">
+            <a href="" className="c-nav-link">
+              <i className="c-icon c-icon-library mr-2"></i>
+              Thư viện
+            </a>
+          </li>
+          <li className="c-nav-item">
+            <a href="" className="c-nav-link">
+              <i className="c-icon c-icon-tutorial mr-2"></i>
+              Hướng dẫn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <main className="c-main">
+      <div className="c-main-left-col">
+        <div className="c-widget">
+          <div className="c-widget-header">Chủ đề nổi bật</div>
+          <div className="c-widget-main">
+            <ul className="c1-nav">
+              <li className="c1-nav-item">
+                <a href="" className="c1-nav-link">
+                  # Học SEO
+                </a>
+              </li>
+              <li className="c1-nav-item">
+                <a href="" className="c1-nav-link">
+                  # Học Wordpress
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="c-widget-header">Tag nổi bật</div>
+          <div className="c-widget-main">
+            <ul className="c1-nav">
+              <li className="c1-nav-item">
+                <a href="" className="c1-nav-link">
+                  # Học SEO
+                </a>
+              </li>
+              <li className="c1-nav-item">
+                <a href="" className="c1-nav-link">
+                  # Học Wordpress
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="c-main-right-col">
+        <div className="c-main-right-left-col c-ml-12px c-mr-12px">
+          <div className="c-post">
+            <article>
+              <header className="c-ml-12px c-mr-12px">
+                <div className="d-flex c-mb-12px">
+                  <span className="c-text text-muted">Tạo bởi&nbsp;</span>
+                  <a
+                    href="https://www.codehub.vn/users/user-m3sszrk"
+                    title="Toàn Mạnh"
+                    className="c1-link"
+                  >
+                    user-m3sszrk
+                  </a>
+                  <span className="c-text text-muted">&nbsp;1 năm trước</span>
+                </div>
+                <div className="d-flex">
+                  <a
+                    href="https://www.codehub.vn/cau-hoi"
+                    title="Hỏi Đáp về Lập Trình Phần Mềm"
+                    className="d-flex my-auto c-mr-6px text-decoration-none"
+                  >
+                    <span className="badge badge-warning c-badge">Câu hỏi</span>
+                  </a>
+                  <h3 className="mb-0 c1-text">
+                    <a
+                      href="https://www.codehub.vn/PHP-Lay-ten-cua-truong-trong-chuoi-truy-van-query-string"
+                      title="PHP - Lấy tên của trường trong chuỗi truy vấn query string"
+                      className="c-link"
+                    >
+                      PHP - Lấy tên của trường trong chuỗi truy vấn query string
+                    </a>
+                  </h3>
+                </div>
+              </header>
+              <main className="c-post-main">
+                <p>
+                  In this blog post, I have compiled some helpful javascript
+                  shorthand coding techniques. Javascript shorthands are good
+                  coding techniques that can help programmers optimize and
+                  simplify their javascript codes.
+                </p>
+                <h2>1. If Presence</h2>
+                <p>
+                  techniques that can help programmers optimize and simplify
+                  their javascript codes.
+                </p>
+                <a
+                  href="https://www.codehub.vn/Web-server-la-gi"
+                  title="Web server là gì"
+                  className="text-decoration-none d-block"
+                >
+                  <img
+                    src="https://img.codehub.vn/post/4cbb37754a88a9ca62ebc75e307bf80c.jpg"
+                    alt="Web server là gì"
+                    className="img-fluid mx-auto d-block"
+                  />
+                </a>
+                <p>
+                  In this blog post, I have compiled some helpful javascript
+                  shorthand coding techniques. Javascript shorthands are good
+                  coding techniques that can help programmers optimize and
+                  simplify their javascript codes.
+                </p>
+                <h2>1. If Presence</h2>
+                <p>
+                  techniques that can help programmers optimize and simplify
+                  their javascript codes.
+                </p>
+              </main>
+              <footer className="c-post-footer">
+                <button
+                  className="btn c-btn-none"
+                  data-post-id="1793"
+                  data-url="https://www.codehub.vn/posts/1793/vote/up"
+                  data-redirect-url="https://www.codehub.vn/Bai-4-Cau-Lenh-SELECT-DISTINCT-Trong-SQL"
+                >
+                  <div className="d-flex">
+                    <svg
+                      className="bi bi-shift m-auto"
+                      width="18px"
+                      height="18px"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M7.27 2.047a1 1 0 011.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H11.5v3a1 1 0 01-1 1h-5a1 1 0 01-1-1v-3H1.654C.78 10.5.326 9.455.924 8.816L7.27 2.047zM14.346 9.5L8 2.731 1.654 9.5H4.5a1 1 0 011 1v3h5v-3a1 1 0 011-1h2.846z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    <span className="c-ml-6px c-text">10 thích</span>
+                  </div>
+                </button>
+                <button
+                  className="btn c-btn-none"
+                  data-post-id="1793"
+                  data-url="https://www.codehub.vn/posts/1793/vote/up"
+                  data-redirect-url="https://www.codehub.vn/Bai-4-Cau-Lenh-SELECT-DISTINCT-Trong-SQL"
+                >
+                  <div className="d-flex">
+                    <span className="c-text">10 bình luận</span>
+                  </div>
+                </button>
+              </footer>
+            </article>
+          </div>
+          <div className="c-post">
+            <article>
+              <header className="c-ml-12px c-mr-12px">
+                <div className="d-flex c-mb-12px">
+                  <span className="c-text text-muted">Tạo bởi&nbsp;</span>
+                  <a
+                    href="https://www.codehub.vn/users/user-m3sszrk"
+                    title="Toàn Mạnh"
+                    className="c1-link"
+                  >
+                    user-m3sszrk
+                  </a>
+                  <span className="c-text text-muted">&nbsp;1 năm trước</span>
+                </div>
+                <div className="d-flex">
+                  <a
+                    href="https://www.codehub.vn/cau-hoi"
+                    title="Hỏi Đáp về Lập Trình Phần Mềm"
+                    className="d-flex my-auto c-mr-6px text-decoration-none"
+                  >
+                    <span className="badge badge-warning c-badge">Câu hỏi</span>
+                  </a>
+                  <h3 className="mb-0 c1-text">
+                    <a
+                      href="https://www.codehub.vn/PHP-Lay-ten-cua-truong-trong-chuoi-truy-van-query-string"
+                      title="PHP - Lấy tên của trường trong chuỗi truy vấn query string"
+                      className="c-link"
+                    >
+                      PHP - Lấy tên của trường trong chuỗi truy vấn query string
+                    </a>
+                  </h3>
+                </div>
+              </header>
+              <main className="c-post-main">
+                <p>
+                  In this blog post, I have compiled some helpful javascript
+                  shorthand coding techniques. Javascript shorthands are good
+                  coding techniques that can help programmers optimize and
+                  simplify their javascript codes.
+                </p>
+                <h2>1. If Presence</h2>
+                <p>
+                  techniques that can help programmers optimize and simplify
+                  their javascript codes.
+                </p>
+                <a
+                  href="https://www.codehub.vn/Web-server-la-gi"
+                  title="Web server là gì"
+                  className="text-decoration-none d-block"
+                >
+                  <img
+                    src="https://img.codehub.vn/post/4cbb37754a88a9ca62ebc75e307bf80c.jpg"
+                    alt="Web server là gì"
+                    className="img-fluid mx-auto d-block"
+                  />
+                </a>
+                <p>
+                  In this blog post, I have compiled some helpful javascript
+                  shorthand coding techniques. Javascript shorthands are good
+                  coding techniques that can help programmers optimize and
+                  simplify their javascript codes.
+                </p>
+                <h2>1. If Presence</h2>
+                <p>
+                  techniques that can help programmers optimize and simplify
+                  their javascript codes.
+                </p>
+              </main>
+              <footer className="c-post-footer">
+                <button
+                  className="btn c-btn-none"
+                  data-post-id="1793"
+                  data-url="https://www.codehub.vn/posts/1793/vote/up"
+                  data-redirect-url="https://www.codehub.vn/Bai-4-Cau-Lenh-SELECT-DISTINCT-Trong-SQL"
+                >
+                  <div className="d-flex">
+                    <svg
+                      className="bi bi-shift m-auto"
+                      width="18px"
+                      height="18px"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M7.27 2.047a1 1 0 011.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H11.5v3a1 1 0 01-1 1h-5a1 1 0 01-1-1v-3H1.654C.78 10.5.326 9.455.924 8.816L7.27 2.047zM14.346 9.5L8 2.731 1.654 9.5H4.5a1 1 0 011 1v3h5v-3a1 1 0 011-1h2.846z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    <span className="c-ml-6px c-text">10 thích</span>
+                  </div>
+                </button>
+                <button
+                  className="btn c-btn-none"
+                  data-post-id="1793"
+                  data-url="https://www.codehub.vn/posts/1793/vote/up"
+                  data-redirect-url="https://www.codehub.vn/Bai-4-Cau-Lenh-SELECT-DISTINCT-Trong-SQL"
+                >
+                  <div className="d-flex">
+                    <span className="c-text">10 bình luận</span>
+                  </div>
+                </button>
+              </footer>
+            </article>
+          </div>
+        </div>
+        <div className="c-main-right-right-col">300x250</div>
+      </div>
+    </main>
+    <footer></footer>
+  </>
+)
